7d0996939884614e453c4ae2fb173bec48fafa2f
Lucas Saldanha <lucascrsaldanha@gmail.com>
2019-04-03 16:08:41 +1300

    Fix typo

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 /* The adress used when sending transactions to the node */
 var address = "0xfe3b557e8fb62b89f4916b721be55ceb828dbd73";
 
-/* The private key associated with the address abode */
+/* The private key associated with the address above */
 var privateKey = "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63";
 
 module.exports = {
