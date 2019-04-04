247864f0452d97c9806e3a2ad0a18de6e18e9d35
Lucas Saldanha <lucascrsaldanha@gmail.com>
2019-04-05 12:25:01 +1300

    Updated readme and Rules migration (#5)
    * Updated readme
    
    * Updated Ingress default contract address

--- migrations/2_deploy_rules_contract.js
+++ migrations/2_deploy_rules_contract.js
@@ -1,5 +1,19 @@
 var Rules = artifacts.require("./Rules.sol");
+var Ingress = artifacts.require("./Ingress.sol");
+
+/*
+	The ingress contract address must match the address for this contract that is being used in the genesis file
+*/
+var ingressContractAddress = "0x0000000000000000000000000000000000009999";
+var rulesContractName = "Rules";
+var rulesVersion = 1000000;
 
 module.exports = function(deployer) {
-  deployer.deploy(Rules);
+  deployer.deploy(Rules).then(function() {
+  	return Ingress.at(ingressContractAddress);
+  }).then(function(ingress){
+  	ingress.registerName(rulesContractName, Rules.address, rulesVersion);
+  }).then(function() {
+  	console.log("   > Updated Ingress contract with Rules name = " + rulesContractName + ", address = " + Rules.address + ", and version = " + rulesVersion);
+  });
 };
