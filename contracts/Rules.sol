07ee34b19a4bf726abde34308d32d5284e187eea
Chris Mckay <chris@error.cm>
2019-04-09 11:27:20 +1000

    wrapping for function parameters

--- contracts/Rules.sol
+++ contracts/Rules.sol
@@ -114,9 +114,15 @@ contract Rules is RulesProxy {
 
     // RULES - IS CONNECTION ALLOWED
     function connectionAllowed(
-        bytes32 sourceEnodeHigh, bytes32 sourceEnodeLow, bytes16 sourceEnodeIp, uint16 sourceEnodePort,
-        bytes32 destinationEnodeHigh, bytes32 destinationEnodeLow, bytes16 destinationEnodeIp, uint16 destinationEnodePort)
-        public view returns (bool) {
+        bytes32 sourceEnodeHigh,
+        bytes32 sourceEnodeLow,
+        bytes16 sourceEnodeIp,
+        uint16 sourceEnodePort,
+        bytes32 destinationEnodeHigh,
+        bytes32 destinationEnodeLow,
+        bytes16 destinationEnodeIp,
+        uint16 destinationEnodePort
+    ) public view returns (bool) {
         return (
             enodeAllowed(
                 sourceEnodeHigh,
