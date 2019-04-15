56a5a8855efdb3c6d22ca2c8fb9723c11fae4179
Sally MacFarlane <sally.macfarlane@consensys.net>
2019-04-15 17:44:38 +1000

    async to get actual ingress contract

--- migrations/2_deploy_ingress_contract.js
+++ migrations/2_deploy_ingress_contract.js
@@ -3,7 +3,7 @@ var Ingress = artifacts.require("./Ingress.sol");
 /* The address of the ingress contract if pre deployed */
 var ingressAddress = process.env.INGRESS_CONTRACT_ADDRESS;
 
-module.exports = (deployer, network) => {
+module.exports = async(deployer, network) => {
     if (! ingressAddress) {
         // Only deploy if we haven't been provided a predeployed address
         deployer.deploy(Ingress).then(() => {
@@ -12,11 +12,12 @@ module.exports = (deployer, network) => {
 
     } else {
         // If supplied an address, make sure there's something there
-        const ingressInstance = Ingress.at(ingressAddress);
+        let ingressInstance = await Ingress.at(ingressAddress);
         try {
-            ingressInstance.getContractVersion();
-            console.log("   > Ingress contract previously initialised at address = " + ingressAddress);
-        } catch {
+            let result = await ingressInstance.getContractVersion();
+            console.log("   > Ingress contract previously initialised at address = " + ingressAddress + " version=" + result);
+        } catch (err) {
+            console.log(err);
             console.error("   > Predeployed Ingress contract is not responding like an Ingress contract at address = " + ingressAddress);
         }
     }
