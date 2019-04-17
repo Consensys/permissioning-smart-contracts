dce1723b409ea56f325d0aa8160c5a28f4f8c6c2
Lucas Saldanha <lucascrsaldanha@gmail.com>
2019-04-17 13:18:32 +1200

    Fix coverage compiler error

--- truffle-config.js
+++ truffle-config.js
@@ -29,8 +29,15 @@ module.exports = {
       port: 7545,
       network_id: '*',
     },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8545,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    }
   },
-  
+
   compilers: {
     solc: {
       version: "0.5.7",
