89eaf4924893bb7113a14f3e41953469bbb1f1ce
Lucas Saldanha <lucas.saldanha@consensys.net>
2019-05-17 10:33:03 +1200

    feat(OP-17): set up rimble ui theme (#48)

--- app/src/App.js
+++ app/src/App.js
@@ -1,35 +1,35 @@
-import React, { Component } from "react";
+// Libs
+import React from "react";
 import { Drizzle, generateStore } from 'drizzle';
 import { DrizzleContext } from "drizzle-react";
+import { ThemeProvider } from "styled-components";
+// Components
 import Main from "./components/Main";
-
-import "./App.css";
-
+// Theme
+import theme from "./constants/theme";
+// Drizzle configuration
 import drizzleOptions from "./drizzleOptions";
-
 const drizzleStore = generateStore(drizzleOptions);
 const drizzle = new Drizzle(drizzleOptions, drizzleStore);
 
-class App extends Component {
-  render() {
-    return (
-      <DrizzleContext.Provider drizzle={drizzle}>
-        <DrizzleContext.Consumer>
-          {drizzleContext => {
-            const { initialized } = drizzleContext;
-        
-            if (!initialized) {
-              return "Loading...";
-            }
+const App = () => (
+  <DrizzleContext.Provider drizzle={drizzle}>
+    <ThemeProvider theme={theme}>
+      <DrizzleContext.Consumer>
+        {drizzleContext => {
+          const { initialized } = drizzleContext;
 
-            return (
-              <Main />
-            );
-          }}
-        </DrizzleContext.Consumer>
-      </DrizzleContext.Provider>
-    );
-  }
-}
+          if (!initialized) {
+            return "Loading...";
+          }
+
+          return (
+            <Main />
+          );
+        }}
+      </DrizzleContext.Consumer>
+    </ThemeProvider>
+  </DrizzleContext.Provider>
+);
 
 export default App;
