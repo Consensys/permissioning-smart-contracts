b286fd7ab72b14db8e6071e31bb88155511b5762
Loïc Vincent-Genod <loic.vincent-genod@consensys.net>
2019-05-24 16:24:44 +0200

    feat(31): add tooltips and pending opacity

---
+++ app/src/components/EnodeTab/TextWithTooltip.js
@@ -0,0 +1,51 @@
+// Libs
+import React from "react";
+import classnames from "classnames";
+// Rimble Components
+import { Tooltip, Text } from "rimble-ui";
+// Constants
+import {
+    PENDING_ADDITION,
+    PENDING_REMOVAL
+} from "../../constants/transactions";
+// Styles
+import styles from "./styles.module.scss";
+
+const TextWithTooltip = ({ status, isReadOnly, isAdmin, text }) => {
+    return status === PENDING_ADDITION ||
+        status === PENDING_REMOVAL ||
+        isReadOnly ||
+        !isAdmin ? (
+        <Tooltip
+            placement="center"
+            message={
+                isReadOnly
+                    ? "Unlock to make changes."
+                    : !isAdmin
+                    ? "You must be an admin to perform modifications."
+                    : "This transaction is pending."
+            }
+            variant="dark"
+        >
+            <Text
+                className={classnames(
+                    styles.ellipsis,
+                    status === PENDING_REMOVAL
+                        ? styles.pendingRemoval
+                        : status === PENDING_ADDITION
+                        ? styles.pendingAddition
+                        : styles.lock
+                )}
+                fontSize="14px"
+            >
+                {text}
+            </Text>
+        </Tooltip>
+    ) : (
+        <Text className={styles.ellipsis} fontSize="14px">
+            {text}
+        </Text>
+    );
+};
+
+export default TextWithTooltip;
