c9b5c659e5f67e7c8ace8d8c1bcaa2688407574f
Chris Mckay <chris@error.cm>
2019-05-28 15:28:42 +1000

    Getting test coverage up

--- contracts/AccountIngress.sol
+++ contracts/AccountIngress.sol
@@ -30,7 +30,7 @@ contract AccountIngress is Ingress {
         bytes memory payload
     ) public view returns (bool) {
         if(getContractAddress(RULES_CONTRACT) == address(0)) {
-            return true;
+            return false;
         }
 
         return AccountRulesProxy(registry[RULES_CONTRACT].contractAddress).transactionAllowed(
