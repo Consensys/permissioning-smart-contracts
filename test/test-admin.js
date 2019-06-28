b2c392fc343cab7d9f117cc7585be47ccf86e4fe
Mark Terry <mark.terry@consensys.net>
2019-06-28 14:32:27 +1000

    [OP-38] Refactored contract for Dapp use.  Fixed some Dapp error handling.

--- test/test-admin.js
+++ test/test-admin.js
@@ -16,7 +16,6 @@ contract("Admin (admin management)", async accounts => {
 
   it("non-deployer account should not be admin", async () => {
     let isAuthorized = await adminContract.isAuthorized(accounts[1]);
-
     assert.notOk(isAuthorized);
   });
 
@@ -33,15 +32,6 @@ contract("Admin (admin management)", async accounts => {
     await adminContract.addAdmin(accounts[2], { from: accounts[0] });
     let isAuthorized = await adminContract.isAuthorized(accounts[2]);
     assert.ok(isAuthorized);
-  });
-
-  it("admin cannot try to add himself", async () => {
-    try {
-      await adminContract.addAdmin(accounts[0], { from: accounts[0] });
-      expect.fail(null, null, "Modifier was not enforced")
-    } catch(err) {
-      expect(err.reason).to.contain("Cannnot invoke method with own account as parameter");
-    }
   });
 
   it("non admin cannot remove another admin", async () => {
@@ -93,5 +83,37 @@ contract("Admin (admin management)", async accounts => {
     await adminContract.removeAdmin(accounts[1], { from: accounts[0] });
     admins = await adminContract.getAdmins.call();
     assert.sameMembers([accounts[0], accounts[2]], admins);
+  });
+
+  it("Should emit events when an Admin is added", async () => {
+    const ownAddress = accounts[0]
+    const address = accounts[1];
+
+    // Add a new account
+    await adminContract.addAdmin(address);
+
+    // Attempt to add a duplicate entry
+    await adminContract.addAdmin(address);
+
+    // Attempt to add self
+    await adminContract.addAdmin(ownAddress);
+    
+    // Get the events
+    let result = await adminContract.getPastEvents("AdminAdded", {fromBlock: 0, toBlock: "latest" });
+    
+    // Verify the first AccountAdded event is 'true'
+    assert.equal(result[0].returnValues.adminAdded, true, "adminAdded SHOULD be true");
+
+    // Verify the unsuccessful duplicate AccountAdded event is 'false'
+    assert.equal(result[1].returnValues.adminAdded, false, "adminAdded with duplicate account SHOULD be false");
+
+    // Verify the unsuccessful duplicate AccountAdded event has correct message
+    assert.equal(result[1].returnValues.message, "Account is already an Admin", "duplicate Admin error message SHOULD be correct");
+
+    // Verify the adding own account AccountAdded event is 'false'
+    assert.equal(result[2].returnValues.adminAdded, false, "adminAdded with own account SHOULD be false");
+
+    // Verify the adding own account AccountAdded event has correct message
+    assert.equal(result[2].returnValues.message, "Adding own account as Admin is not permitted", "adding self Admin error message SHOULD be correct");
   });
 });
