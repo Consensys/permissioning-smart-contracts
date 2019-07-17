6d07ca074f5e6f1ae8d1cf9f4988286a01b5561a
Sally MacFarlane <sally.macfarlane@consensys.net>
2019-07-18 09:40:53 +1000

    [OP-54] remove subtexts (#110)
    * remove subtexts

--- src/constants/modals.ts
+++ src/constants/modals.ts
@@ -15,7 +15,7 @@ export const addAdminDisplay: ModalDisplay = {
   inputPlaceholder: 'Ex: 0xAc03BB73b6a9e108530AFf4Df5077c2B3D481e5A',
   label: 'Account Address',
   heading: 'Add Admin Account',
-  subHeading: 'Admin accounts can...'
+  subHeading: ''
 };
 
 export const removeAdminDisplay: (value: string | boolean) => ModalDisplay = value => ({
@@ -29,7 +29,7 @@ export const addAccountDisplay: ModalDisplay = {
   inputPlaceholder: 'Ex: 0xAc03BB73b6a9e108530AFf4Df5077c2B3D481e5A',
   label: 'Account Address',
   heading: 'Add Whitelisted Account',
-  subHeading: 'Whitelisted accounts may submit transactions on the network.'
+  subHeading: ''
 };
 
 export const removeAccountDisplay: (value: string | boolean) => ModalDisplay = value => ({
@@ -44,7 +44,7 @@ export const addEnodeDisplay: ModalDisplay = {
     'Ex: enode://72b0d3ee9e86e072cca078b2588163bf8d9b85fa93923a31f4b97d13cf5280b3d32de9c13d4b7e3cc615d8c1347c97da760a689fac05d9ec80bda4517015ee78@127.0.0.1:30304',
   label: 'Enode URL',
   heading: 'Add Whitelisted Node',
-  subHeading: 'Nodes can connect to each other if they are both whitelisted. See formatting details here.'
+  subHeading: ''
 };
 
 export const removeEnodeDisplay: (value: string) => ModalDisplay = value => ({
