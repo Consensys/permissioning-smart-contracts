1bdd2e9f6108c65dbb7db7b9998b083afe248d52
Sally MacFarlane <sally.macfarlane@consensys.net>
2019-07-18 13:32:32 +1000

    [MINOR] wording changes (#112)
    * correct -> valid
    
    * Address -> Node ID

--- src/constants/modals.ts
+++ src/constants/modals.ts
@@ -11,7 +11,7 @@ export type ModalDisplay = {
 
 export const addAdminDisplay: ModalDisplay = {
   submitText: 'Add Admin Account',
-  errorMessage: 'Account address is not correct.',
+  errorMessage: 'Account address is not valid.',
   inputPlaceholder: 'Ex: 0xAc03BB73b6a9e108530AFf4Df5077c2B3D481e5A',
   label: 'Account Address',
   heading: 'Add Admin Account',
@@ -25,7 +25,7 @@ export const removeAdminDisplay: (value: string | boolean) => ModalDisplay = val
 
 export const addAccountDisplay: ModalDisplay = {
   submitText: 'Add Whitelisted Account',
-  errorMessage: 'Account address is not correct.',
+  errorMessage: 'Account address is not valid.',
   inputPlaceholder: 'Ex: 0xAc03BB73b6a9e108530AFf4Df5077c2B3D481e5A',
   label: 'Account Address',
   heading: 'Add Whitelisted Account',
@@ -39,7 +39,7 @@ export const removeAccountDisplay: (value: string | boolean) => ModalDisplay = v
 
 export const addEnodeDisplay: ModalDisplay = {
   submitText: 'Add Whitelisted Node',
-  errorMessage: 'Enode URL must include Address, IP address and Port.',
+  errorMessage: 'Enode URL must include valid Node ID, IP address and Port.',
   inputPlaceholder:
     'Ex: enode://72b0d3ee9e86e072cca078b2588163bf8d9b85fa93923a31f4b97d13cf5280b3d32de9c13d4b7e3cc615d8c1347c97da760a689fac05d9ec80bda4517015ee78@127.0.0.1:30304',
   label: 'Enode URL',
