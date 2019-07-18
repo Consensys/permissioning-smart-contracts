cc5e1ef48470ad213e3551684992fcdde66b62a8
Sally MacFarlane <sally.macfarlane@consensys.net>
2019-07-18 12:20:48 +1000

    [OP-55] remove tooltip (#111)
    * removed tooltip

--- src/components/AccountTab/TableHeader.tsx
+++ src/components/AccountTab/TableHeader.tsx
@@ -2,9 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // Rimble Components
-import { Flex, Box, Heading, Button, Icon, Tooltip } from 'rimble-ui';
-// Styles
-import styles from './styles.module.scss';
+import { Flex, Box, Heading, Button } from 'rimble-ui';
 
 type TableHeader = {
   number: Number;
@@ -16,9 +14,6 @@ const TableHeader: React.FC<TableHeader> = ({ number, openAddModal, disabledAdd 
   <Flex alignItems="center" justifyContent="space-between">
     <Box display="flex" alignItems="center">
       <Heading.h2 fontWeight="700">Whitelisted Accounts ({number})</Heading.h2>
-      <Tooltip message="Whitelisted accounts may submit transactions on the network." placement="bottom" variant="dark">
-        <Icon ml={3} name="InfoOutline" className={styles.infoIcon} />
-      </Tooltip>
     </Box>
     <Flex alignItems="center">
       <Button icon="AddCircleOutline" mainColor="#25D78F" onClick={() => openAddModal()} disabled={disabledAdd}>
