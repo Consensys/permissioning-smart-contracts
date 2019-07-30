a78df6bd1656d8e09fa25fd0ed914ad34a51a64b
Chris Mckay <chris@error.cm>
2019-07-30 22:27:55 +1000

    should work on dash

--- scripts/wrap_env.sh
+++ scripts/wrap_env.sh
@@ -1,6 +1,6 @@
 # Load up .env
 set -o allexport
-[[ -f .env ]] && source .env
+if [ -f .env ]; then source .env; fi
 set +o allexport
 export REACT_APP_ACCOUNT_INGRESS_CONTRACT_ADDRESS=$ACCOUNT_INGRESS_CONTRACT_ADDRESS
 export REACT_APP_NODE_INGRESS_CONTRACT_ADDRESS=$NODE_INGRESS_CONTRACT_ADDRESS
