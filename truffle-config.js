0713a9b5cb11e7004da96eec91fe0fd083facba2
Sally MacFarlane <sally.macfarlane@consensys.net>
2019-09-25 10:48:55 +1000

    pantheon -> besu
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- truffle-config.js
+++ truffle-config.js
@@ -5,13 +5,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 /* The adress used when sending transactions to the node */
-var address = process.env.PANTHEON_NODE_PERM_ACCOUNT;
+var address = process.env.BESU_NODE_PERM_ACCOUNT;
 
 /* The private key associated with the address above */
-var privateKey = process.env.PANTHEON_NODE_PERM_KEY;
+var privateKey = process.env.BESU_NODE_PERM_KEY;
 
 /* The endpoint of the Ethereum node */
-var endpoint = process.env.PANTHEON_NODE_PERM_ENDPOINT;
+var endpoint = process.env.BESU_NODE_PERM_ENDPOINT;
 if (endpoint === undefined) {
   endpoint = "http://127.0.0.1:8545";
 }
