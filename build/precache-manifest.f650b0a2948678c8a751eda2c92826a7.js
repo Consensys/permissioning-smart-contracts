6c8b1e7d6206578af7f3fc008e46083cbf1ef538
JMarchettiSabra <jmarchetti@gruposabra.com>
2021-05-10 18:11:58 -0300

    build directory

---
+++ build/precache-manifest.f650b0a2948678c8a751eda2c92826a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7972f60483bb3122046281f4964f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841b8b6abaa5ce9531c4",
+    "url": "/static/css/main.5f008376.chunk.css"
+  },
+  {
+    "revision": "0be60f01a3160de0d73d",
+    "url": "/static/js/2.7a2de43d.chunk.js"
+  },
+  {
+    "revision": "841b8b6abaa5ce9531c4",
+    "url": "/static/js/main.e571573b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d083e4dd5a39b5ad417369d891c1f6f4",
+    "url": "/static/media/QuorumLogo_Blue.d083e4dd.png"
+  }
+]);
