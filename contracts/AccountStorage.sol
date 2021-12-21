45702dd4b5df38459138e6d6c55fc484a12e0ff2
Alberto Hernandez <39121047+albert0-hernandez@users.noreply.github.com>
2021-12-21 23:50:37 +0100

    Upgrade Pragma Header in order to allow solidity latest solidity compilers (#261)
    * Upgrade Pragma Header in order to allow solidity latest solidity compilers
    
    * Upgrade minimum version of contracts to 0.7.0
    Pragma header mandatory added
    Some minor changes on SC according the upgrade of Solidity
    
    * Fix Lint Problems
    
    * Downgrade Truffle because latest version does not generate the test reports
    
    * Downgrade the smartcontract compatibility to 0.6.0
    
    * Restore accidentally deleted files
    
    Co-authored-by: Alberto Hernandez <albherna@gmail.com>

--- contracts/AccountStorage.sol
+++ contracts/AccountStorage.sol
@@ -1,4 +1,18 @@
-pragma solidity 0.5.9;
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+pragma solidity >=0.6.0 <0.9.0;
 
 import "./Admin.sol";
 import "./AccountIngress.sol";
@@ -55,7 +69,8 @@ contract AccountStorage {
 
     function add(address _account) public onlyLatestVersion returns (bool) {
         if (indexOf[_account] == 0) {
-            indexOf[_account] = allowlist.push(_account);
+            allowlist.push(_account);
+            indexOf[_account] = allowlist.length;
             return true;
         }
         return false;
@@ -72,7 +87,7 @@ contract AccountStorage {
             }
 
             //shrink array
-            allowlist.length -= 1; // mythx-disable-line SWC-101
+            allowlist.pop();
             indexOf[_account] = 0;
             return true;
         }
